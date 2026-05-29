8bdc4e1516c434f7704934eb9367af585f34799f
Zeeshan Ullah <zeeshanullah.freelancer@gmail.com>
2026-05-29 20:26:29 +0500

    Custome Theme Added

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-light-300">
+      <Text className="text-2xl text-blue-600">Welcome to Nativewind</Text>
+    </View>
+  );
+}
